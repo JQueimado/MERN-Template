db82f0350397fed30c7506a54e2b136ccadc8a11
JQueimado <jpqueimado@gmail.com>
2022-01-28 18:38:34 +0000

    change todo to template

--- server.js
+++ server.js
@@ -4,28 +4,30 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 3000;
-
 require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = process.env.ATLAS_URI;
-mongose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
-const connection = mongose.connection;
 
-connection.once("open", () => {
-  console.log("Connected to Atlas");
-});
+/*
+* const uri = process.env.ATLAS_URI;
+* mongose.connect(uri, {
+*   useNewUrlParser: true,
+*   useCreateIndex: true,
+*   useUnifiedTopology: true
+* });
+* const connection = mongose.connection;
+*
+* connection.once("open", () => {
+*   console.log("Connected to Atlas");
+* });
+*/
 
 //Routes
-const todo = require("./template/template.route");
+const template = require("./template/template.route");
 
-app.use("/template", todo);
+app.use("/template", template);
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
